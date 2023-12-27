2c3b40942d4ee3b232a8fa78d757c7d897b880f9
lucassarto <lucas_sarto@outlook.com>
2023-12-26 21:44:15 -0300

    Learning Python
    Hi Dear! In first step i am using variable to get somes input of data from users!

---
+++ Desafio2.py
@@ -0,0 +1,6 @@
+
+
+dia = input ('Dia = ')
+mes = input ('Mes = ')
+ano = input ('Ano = ')
+print('você nasceu no dia',dia,'de',mes ,'de', ano,'.', 'Correto?')
---
+++ Desafio3.py
@@ -0,0 +1,4 @@
+num1 = input('insira o primeiro número')
+num2 = input('insira o segundo número')
+num3= int(num1)  + int(num2)
+print('a soma é', num3)
